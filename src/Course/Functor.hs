32584b2e2ef5be946d51ce0f25d86292100825d2
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-05-09 16:45:37 +0300

    Functor.hs exercises completed

--- src/Course/Functor.hs
+++ src/Course/Functor.hs
@@ -28,8 +28,7 @@ infixl 4 <$>
 -- >>> (+1) <$> Id 2
 -- Id 3
 instance Functor Id where
-  (<$>) =
-    error "todo"
+  (<$>) g (Id a) = Id (g a)
 
 -- | Maps a function on the List functor.
 --
@@ -39,8 +38,7 @@ instance Functor Id where
 -- >>> (+1) <$> (1 :. 2 :. 3 :. Nil)
 -- [2,3,4]
 instance Functor List where
-  (<$>) =
-    error "todo"
+  (<$>) = map
 
 -- | Maps a function on the Optional functor.
 --
@@ -50,16 +48,14 @@ instance Functor List where
 -- >>> (+1) <$> Full 2
 -- Full 3
 instance Functor Optional where
-  (<$>) =
-    error "todo"
+  (<$>) = mapOptional
 
 -- | Maps a function on the reader ((->) t) functor.
 --
 -- >>> ((+1) <$> (*2)) 8
 -- 17
 instance Functor ((->) t) where
-  (<$>) =
-    error "todo"
+  (<$>) = (.)
 
 -- | Anonymous map. Maps a constant value on a functor.
 --
@@ -74,8 +70,7 @@ instance Functor ((->) t) where
   a
   -> f b
   -> f a
-(<$) =
-  error "todo"
+(<$) a f = const a <$> f
 
 -- | Anonymous map producing unit value.
 --
@@ -94,8 +89,7 @@ void ::
   Functor f =>
   f a
   -> f ()
-void =
-  error "todo"
+void = (<$>) (const ())
 
 -----------------------
 -- SUPPORT LIBRARIES --
